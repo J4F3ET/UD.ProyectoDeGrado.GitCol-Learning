fecfb29577052b15ddb3dca09be6928d03899f6d
J4F3ET <jslopezz@udistrital.edu.co>
2023-12-23 12:52:20 -0500

    Agregando Observers

--- src/controller/loginController.js
+++ src/controller/loginController.js
@@ -1,11 +1,17 @@
 import {Router} from "express";
 import {releaseVerificationMiddleware} from "./util/middleware .js";
+import {ejerciseCreate, ejerciseFindById} from "../model/ejerciseService.js";
 const router = Router();
-router.get("/login", (req, res) => {
+router.get("/login", async (req, res) => {
+	const idKey = ejerciseCreate("Ejercicio 1", "Ejercicio 1", "Ejercicio 1", "Ejercicio 1");
+	//const find = ejerciseFindById("1");
+	console.log(await idKey);
+	//console.log(await find);
 	res.render("login");
 });
 router.post("/login", releaseVerificationMiddleware, (req, res) => {
-	res.status(200).json({message: "OK"});
+	// Agregar Servicio "Verificar si el usuario no se encuentra en una sala"
+	res.status(200).json({url: "/rooms"});
 	res.end()
 });
 router.get("/rooms", releaseVerificationMiddleware, (req, res) => {
---
+++ src/view/dist/js/userNotAuthObserver.js
@@ -0,0 +1,21 @@
+import {auth} from "./firebase.js";
+import {onAuthStateChanged} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+export async function login(user) {
+	const headers = new Headers();
+	headers.append("Content-Type", "application/json");
+	headers.append("Authorization", `Bearer ${user.accessToken}`);
+	return fetch("/login", {
+		method: "POST",
+		headers: headers,
+	});
+}
+onAuthStateChanged(auth, async (user) => {
+	if (!user) return;
+	const response = await login(user);
+	const responseJson = response.json();
+	if (response.status === 200) {
+		window.location.href = (await responseJson).url;
+	}else{
+		alert("Login failed");
+	}
+});
