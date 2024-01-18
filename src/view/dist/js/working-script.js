f54318c6f94bc0a4f64ddd55a8e0646fa48ee8ac
J4F3ET <jslopezz@udistrital.edu.co>
2024-01-18 15:39:38 -0500

    agregando showModal para alone

--- src/view/dist/js/working-script.js
+++ src/view/dist/js/working-script.js
@@ -1,3 +1,8 @@
 document.getElementById("team").addEventListener("click",()=>{
     window.location.href = "login";
 });
+const dialogSelectLevel = document.getElementById("dialogSelectLevel");
+const dialogSelectChallenge = document.getElementById("dialogSelectChallenge");
+document.getElementById("alone").addEventListener("click",()=>{
+    dialogSelectLevel.showModal();
+});
