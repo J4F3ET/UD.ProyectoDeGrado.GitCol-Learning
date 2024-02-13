72c07420e290a6db0e4a6d6745ca868a300d2d0d
J4F3ET <jslopezz@udistrital.edu.co>
2024-02-13 15:17:42 -0500

    Agregando nueva rama para modulo de todo lo que contiene SVG

--- src/view/dist/js/comandManager/modules/Commit.js
+++ src/view/dist/js/comandManager/modules/Commit.js
@@ -1,16 +1,21 @@
 export class Commit {
-    constructor() {
+
+    /**
+     * @name constructor
+     * @description Create a new instance of the Commit class
+     * @param {NameRepository} nameRepository Name of the repository to be used in the local storage
+     */
+    constructor(nameRepository) {
+        this._repository = nameRepository;
         this.comand = 'commit';
     }
-
-
     execute(config) {
-    console.log('Commit');
+        
     }
     /**
      * @name createCod
-     * @description Crea un codigo de 7 caracteres aleatorios
-     * @returns {string} Codigo de 7 caracteres aleatorios
+     * @description Create a random code of 7 characters
+     * @returns {string} Returns a random code of 7 characters
      */
     createCod() {
         const caracteres = 'abcdefghijklmnopqrstuvwxyz0123456789';
@@ -20,5 +25,43 @@ export class Commit {
             codigo += caracteres.charAt(indice);
         }
         return codigo;
+    }
+    /**
+     * @name createCommit
+     * @description Create commit in the repository and save it in the local storage
+     * @param {String} message Commit message
+     */
+    createComit(message){
+    }
+    /**
+     * @name addCommitToLocalStorage
+     * @description Add commit to the local storage
+     * @param {JSON} newCommit New commit to be added
+     * @throws {Error} Repository not found
+     * @throws {Error} Error adding commit to the repository
+     */
+    addCommitToLocalStorage(newCommit){
+        if(localStorage.getItem(this._repository)===null)
+            throw new Error('Repository not found');
+        const repository = JSON.parse(localStorage.getItem(this._repository));
+        repository.push(newCommit);
+        localStorage.setItem('repository',JSON.stringify(repository));
+    }
+    /**
+     * @name existsCommitToLocalStorage
+     * @description Check if the commit exists in the local storage
+     * @param {JSON} commit Commit to be checked
+     * @returns {Boolean} Returns true if the commit exists in the local storage, otherwise returns false
+     * @throws {Error} Repository not found
+     */
+    existsCommitToLocalStorage(commit){
+        if(localStorage.getItem(this._repository)===null)
+            throw new Error('Repository not found');
+        const repository = JSON.parse(localStorage.getItem(this._repository));
+        repository.find(_commit => _commit.id === commit.id);
+        if(repository)
+            return false;
+        else
+            return true;
     }
 }
