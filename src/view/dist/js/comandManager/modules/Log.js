78ba44de8a37e928d7c4ce310af767128b3eeeff
J4F3ET <jslopezz@udistrital.edu.co>
2024-03-24 15:35:40 -0500

    class Log: Agregando nuevo modulo

---
+++ src/view/dist/js/comandManager/modules/Log.js
@@ -0,0 +1,33 @@
+export class Log{
+    constructor(repositoryName = 'repository',logRepository = 'log'){
+        this.comand = 'log';
+        this.repositoryName = repositoryName;
+        this.logRepository = logRepository;
+    }
+    execute(){
+        let storage = JSON.parse(localStorage.getItem(this.repositoryName));
+        let message = `<h5 class="help">Log</h5>`;
+        storage.commits.forEach((commit)=>{
+            let tags = '';
+            commit.tags.forEach((tag)=>{
+                if(tag === 'HEAD'){
+                    const head = storage.information.head.includes('detached')?storage.information.head.split(' ').pop():storage.information.head;
+                    tags += `HEAD&nbsp;->&nbsp;${head},&nbsp`;
+                }else if(tag !== storage.information.head)
+                    tags += `${tag},&nbsp`;
+            });
+            message += `<p class="help">commit ${commit.id}&nbsp;(${tags})</p>`;
+            message += `<p class="help">Author:&nbsp;${commit.author?commit.author.name:undefined}&nbsp;&lt;${commit.author?commit.author.email:undefined}&gt;</p>`;
+            message += `<p class="help">Date:&nbsp;&nbsp;&nbsp;${commit.date}</p>`;
+            message += `<p class="help">${commit.message}</p>`;
+        });
+        this.createMessage("info",message);
+    }
+    createMessage(tag,message){
+        if(localStorage.getItem(this.logRepository)===null)
+            localStorage.setItem(this.logRepository,JSON.stringify([]));
+        const log = JSON.parse(localStorage.getItem(this.logRepository));
+        log.push({tag,message});
+        localStorage.setItem(this.logRepository,JSON.stringify(log));
+    }
+}
--- src/view/dist/js/comandManager/working-alone-comandManager.js
+++ src/view/dist/js/comandManager/working-alone-comandManager.js
@@ -3,6 +3,7 @@ import {Commit} from "./modules/Commit.js";
 import {Init} from "./modules/Init.js";
 import {Branch} from "./modules/Branch.js";
 import {Checkout} from "./modules/Checkout.js";
+import {Log} from "./modules/Log.js";
 const REPOSITORYNAME = "local"
 const LOGNAME = "log"
 const workingAloneComandManager = new ComandManager();
@@ -12,4 +13,5 @@ workingAloneComandManager.addComand("init",new Init(REPOSITORYNAME));
 workingAloneComandManager.addComand("commit",new Commit(REPOSITORYNAME,LOGNAME));
 workingAloneComandManager.addComand("checkout",new Checkout(REPOSITORYNAME,LOGNAME));
 workingAloneComandManager.addComand("branch",new Branch(REPOSITORYNAME,LOGNAME,null));
+workingAloneComandManager.addComand("log",new Log(REPOSITORYNAME,LOGNAME));
 export { workingAloneComandManager };
