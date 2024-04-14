b75a6517af319a4c7ccec3b31b4dc0a2e788e7d7
J4F3ET <jslopezz@udistrital.edu.co>
2024-04-13 21:36:02 -0500

    fix: :construction: Corrected use to observer

--- src/view/dist/js/userAuth-observer.js
+++ src/view/dist/js/userAuth-observer.js
@@ -1,18 +1,18 @@
 import {auth} from "./firebase-config.js";
 import {onAuthStateChanged} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 export async function logout() {
-	auth.signOut();
-	return fetch("/logout", {
+	const response = fetch("/logout", {
 		method: "GET",
 		headers: {
 			"Content-Type": "application/json",
 		}
 	});
+	auth.signOut();
+	return response;
 }
 onAuthStateChanged(auth, (user) => {
 	if (user) return;
 	logout().then((response) => {
-		if (response.status !== 401) return;
 		response.json().then((data) => {
 			window.location.href = data.url;
 		});
