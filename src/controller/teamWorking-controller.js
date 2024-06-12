540ac49078e2d9553047b0ebef2e7e616d02e4b7
J4F3ET <jslopezz@udistrital.edu.co>
2024-06-12 10:19:07 -0500

    feat: fire: Update name screen teamWorking to multi-mode

--- src/controller/teamWorking-controller.js
+++ src/controller/teamWorking-controller.js
@@ -9,7 +9,7 @@ const router = Router();
  * /teamWorking:
  *   get:
  *     summary: Endpoint para obtener la pantalla de trabajo en equipo.
- *     description: Retorna la pantalla de trabajo en equipo (teamWorking-screen).
+ *     description: Retorna la pantalla de trabajo en equipo (multi-mode-screen).
  *     parameters:
  *       - in: query
  *         name: room
@@ -23,13 +23,13 @@ const router = Router();
  *         description: Éxito. Retorna la pantalla de trabajo en equipo.
  *         content:
  *           text/html:
- *             example: teamWorking-screen.ejs
+ *             example: multi-mode-screen.ejs
  *     security:
  *       - bearerAuth: []
  *       - cookieAuth: []
  */
 router.get("/teamWorking*",releaseVerificationMiddleware,verifyUserInRoomMiddleware,(req, res) => {
-    res.render("teamWorking-screen", { room: req.query.room });
+    res.render("multi-mode-screen", { room: req.query.room });
 });
 /**
  * @openapi
