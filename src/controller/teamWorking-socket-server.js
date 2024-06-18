cd5610696944a09bdc84c68354d15177ba7c8e78
J4F3ET <jslopezz@udistrital.edu.co>
2024-06-17 21:55:38 -0500

    feat: :sparkles: Update socket handling for teamWorking-socket-server.js

--- src/controller/teamWorking-socket-server.js
+++ src/controller/teamWorking-socket-server.js
@@ -1,11 +1,69 @@
-import { verifyUrl } from "./util/teamWorking-middleware";
-export const socketRoomController = (io) => {
-    io.on('connection', (socket) => {
-        io.use(verifyUrl);
-        socket.join(socket.request.headers.referer.split("=")[1])
-        socket.on('disconnect', () => {
-            socket.leave(socket.request.headers.referer.split("=")[1])
+import {auth} from "../model/firebase-service.js";
+import {
+    roomGet,
+	roomGetByCode,
+    roomUpdate
+} from "../model/room-service.js";
+export class SocketHandler {
+    constructor(io){
+        this._io = io;
+        io.on('connect', (socket) => {
+            // Middleware
+            io.use(this.verifyUrl);
+            const room = socket.request.headers.referer.split("=")[1];
+            if (undefined === room)
+                return Error("Room not found");
+            //Events
+            this.addSocketToRoom(room,socket);
+            this.setupSocketEvents(room,socket);
+            console.log('Connected to server');
         });
-        io.to(socket.request.headers.referer.split("=")[1]).emit('message', 'Hello everyone!'+ socket.request.headers.referer.split("=")[1]);
-    });
+    }
+    verifyUrl = (socket, next) => {
+        const room = socket.request.headers.referer.split("=")[1];
+        const cookie = socket.request.headers.cookie.split("=")[1];
+        if(room == undefined || cookie == undefined){
+            next(new Error("Room not found in url or user not logged in"));
+            return;
+        }
+        auth.verifyIdToken(cookie).then((data) => {
+            roomGet(room).then((_room) => {
+                if(_room.val().members.includes(data.uid)){
+                    next();
+                }else{
+                    next(new Error("Member not found in room"));
+                }
+            }).catch((error) => {
+                next(new Error(error,"User not found in room"));
+            });
+        }).catch((error) => {
+            next(new Error(error,"User not found in room"));
+        });
+    }
+    async updateRepository(data){
+        const room = await roomGetByCode(this.roomCode);
+        if (room === null)
+            return Error("Room not found");
+        console.log("Room found: ", room);
+        
+    }
+    async setupSocketEvents(room,socket){
+        socket.on('disconnect', () => {
+            this.removeSocketFromRoom(room, socket);
+        });
+        socket.on('updateRepository', (args) => {
+            this.updateSocketRepository(socket,args)
+        });
+    };
+    async addSocketToRoom(room,socket){
+        socket.join(room);
+        this._io.to(room).emit('message', 'Teagregamos a '+ room);
+    };
+    async removeSocketFromRoom(room,socket){
+        socket.leave(room);
+    }
+    async updateSocketRepository(socket,data){
+        console.log("Updating repository");
+        socket.emit('message', data);
+    }
 }
--- src/controller/util/teamWorking-middleware.js
+++ src/controller/util/teamWorking-middleware.js
@@ -31,27 +31,3 @@ export const verifyUserInRoomMiddleware = async (req, res, next) => {
     }
     
 }
-// Middleware Socket.io: Funciones que se ejecutan antes de que lleguen a las conexiones
-
-export const verifyUrl = (socket, next) => {
-    if(
-        socket.request.headers.referer.split("=")[1] == undefined || 
-        socket.request.headers.cookie == undefined
-    ){
-        next(new Error("No se ha iniciado sesión"));
-        return;
-    }
-    auth.verifyIdToken(socket.request.headers.cookie.split("=")[1]).then((data) => {
-        roomGet(socket.request.headers.referer.split("=")[1]).then((room) => {
-            if(room.val().members.includes(data.uid)){
-                next();
-            }else{
-                next(new Error("No se encuentra en la sala de trabajo en equipo"));
-            }
-        }).catch((error) => {
-            next(new Error(error,"No se ha iniciado sesión"));
-        });
-    }).catch((error) => {
-        next(new Error(error,"No se ha iniciado sesión"));
-    });
-}
