cd65529f9f5c756b8d868d2d66f8d1364a0e19ae
J4F3ET <jslopezz@udistrital.edu.co>
2024-08-08 15:33:05 -0500

    feat: :fire: Remove button bomb

--- src/view/dist/js/mode-script.js
+++ src/view/dist/js/mode-script.js
@@ -95,12 +95,11 @@ observerScrollSvgHorizontal.observe(containerSvg, { childList: true, subtree: tr
 
 // MODULE MULTI-MODE
 const containerSvgCloud = document.getElementById("svgContainerCloud");
-const observerCloud = new Observer();
 if(containerSvgCloud){
+    const observerCloud = new Observer();
     const dataViewerCloud = new DataViewer(document.getElementById("svgContainerCloud"));
     observerCloud.subscribe(dataViewerCloud);
     observerCloud.notify(sessionStorage.getItem(REF_STORAGE_REPOSITORY_CLOUD));
     const observerScrollSvgHorizontalCloud = new MutationObserver(()=>containerSvgCloud.scrollLeft = containerSvgCloud.scrollWidth)
     observerScrollSvgHorizontalCloud.observe(containerSvgCloud, { childList: true, subtree: true });
 }
-export { observerCloud }
--- src/view/dist/js/multi-mode-socket-client.js
+++ src/view/dist/js/multi-mode-socket-client.js
@@ -1,10 +1,6 @@
 import { SocketHandler } from "./comandManager/SocketHandler.js";
-import { observerCloud} from "./mode-script.js";
-const socketManager = new SocketHandler(REF_STORAGE_REPOSITORY_CLOUD,observerCloud);
 
 document.getElementById('btnLeaveToRoom').addEventListener('click', () => {
-    socketManager.disconnect();
+    new SocketHandler(REF_STORAGE_REPOSITORY_CLOUD).disconnect();
 });
-document.getElementById('btnPush').addEventListener('click', () => {
-    socketManager.sendUpdateRepository(JSON.parse(sessionStorage.getItem(REF_STORAGE_REPOSITORY)));
-});
+
