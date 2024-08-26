d8da3aeddc98314bcef538e8dcf793988eb69afe
J4F3ET <jslopezz@udistrital.edu.co>
2024-08-25 21:20:05 -0500

    doc: :memo: Implement Swagger theme

--- docs/swagger.js
+++ docs/swagger.js
@@ -1,6 +1,11 @@
 import swaggerJSDoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
-
+import { SwaggerTheme, SwaggerThemeNameEnum } from 'swagger-themes';
+const theme = new SwaggerTheme();
+const optionsSwagger = {
+    explorer: false,
+    customCss: theme.getBuffer(SwaggerThemeNameEnum.DARK)
+  };
 // Metadatos de la api
 const options = {
     definition: {
@@ -17,7 +22,7 @@ const options = {
 const swaggerSpec = swaggerJSDoc(options);
 export const swaggerDoc = (app,port) => {
     // Ruta para ver la documentacion
-    app.use("/api-docs",swaggerUi.serve,swaggerUi.setup(swaggerSpec));
+    app.use("/api-docs",swaggerUi.serve,swaggerUi.setup(swaggerSpec,optionsSwagger));
     app.get("/api-docs.json",(req,res) => {
         res.setHeader("Content-Type","application/json");
         res.send(swaggerSpec);
