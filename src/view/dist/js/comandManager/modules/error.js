1ba5c9308ca280c9429c7e65e52613ef1b5cbee0
J4F3ET <jslopezz@udistrital.edu.co>
2024-09-17 15:24:42 -0500

    feat: :sparkles: New function errorNotInitialized and errorNotConfiguration

--- src/view/dist/js/comandManager/modules/error.js
+++ src/view/dist/js/comandManager/modules/error.js
@@ -27,3 +27,30 @@ export class ErrorModule extends Error{
      */
     get message(){return this.message}
 }
+/**
+ * @name errorNotInitialized
+ * @description Create a new error for the command not initialized
+ * @param {String} module Name of the module
+ * @returns {ErrorModule}
+ */
+export const errorNotInitialized = (module)=>{
+    return new ErrorModule(
+        module,
+        'The repository is not initialized',
+        `The repository is not initialized, please execute the command 'git init'`
+    );
+}
+/**
+ * @name errorNotConfiguration
+ * @description Create a new error for the command configuration not valid
+ * @param {String} module Name of the module
+ * @param {String} command Data of the command
+ * @returns {ErrorModule}
+ */
+export const errorNotConfiguration = (module,command)=>{
+    return new ErrorModule(
+        module,
+        `The configuration "${command}" is not valid`,
+        `Try use the command 'git ${module} -h' for more information`
+    );
+}
