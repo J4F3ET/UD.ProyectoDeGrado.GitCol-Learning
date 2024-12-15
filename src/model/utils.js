fa41317a19b354f2cefaa9d99715313a258af4bb
J4F3ET <jslopezz@udistrital.edu.co>
2024-12-15 17:21:00 -0500

    feat: :fire: Change dontenv to native nodejs

--- src/model/utils.js
+++ src/model/utils.js
@@ -1,40 +1,55 @@
+
+process.loadEnvFile();
+
+const {
+	GOOGLE_APPLICATION_CREDENTIALS: firebaseConfig = "",
+	FIREBASE_DATABASE_URL: databaseURL = "",
+} = process.env;
+
+export const envConfig = {
+	firebaseConfig,
+	databaseURL,
+};
+
 export const defaultRepository = async (key) => {
-    return {
-        commits:[],
-        information:{
-            head: null,
-            repository:`origin${key}`,
-        }
-    }
+	return {
+		commits: [],
+		information: {
+			head: null,
+			repository: `origin${key}`,
+		},
+	};
 };
 export async function parseToCommitObject(data) {
-    return {
-        id: data.id || '',
-        message: data.message || '',
-        parent: data.parent || '',
-        unions: data.unions || [],
-        tags: data.tags || [],
-        class: data.class || ['commit'],
-        autor: data.hasOwnProperty('autor') ? data.autor : null,
-        date: data.date || '',
-        cx: data.cx || 0,
-        cy: data.cy || 0
-    };
+	return {
+		id: data.id || "",
+		message: data.message || "",
+		parent: data.parent || "",
+		unions: data.unions || [],
+		tags: data.tags || [],
+		class: data.class || ["commit"],
+		autor: data.hasOwnProperty("autor") ? data.autor : null,
+		date: data.date || "",
+		cx: data.cx || 0,
+		cy: data.cy || 0,
+	};
 }
-export async function parseToRoomObject(key,data) {
-    if(data.repository.hasOwnProperty("commits"))
-        data.repository.commits = await Promise.all(data.repository.commits.map(parseToCommitObject))
+export async function parseToRoomObject(key, data) {
+	if (data.repository.hasOwnProperty("commits"))
+		data.repository.commits = await Promise.all(
+			data.repository.commits.map(parseToCommitObject)
+		);
 
-    const repository = data.repository.hasOwnProperty("commits")
-        ?data.repository
-        :defaultRepository(key)
-    return {
-        code: data.code || '',
-        description: data.description || '',
-        owner: data.owner || '',
-        members: data.members || [],
-        status: data.status || true,
-        repository:await repository,
-        hidden: data.hidden|| false,
-    };
+	const repository = data.repository.hasOwnProperty("commits")
+		? data.repository
+		: defaultRepository(key);
+	return {
+		code: data.code || "",
+		description: data.description || "",
+		owner: data.owner || "",
+		members: data.members || [],
+		status: data.status || true,
+		repository: await repository,
+		hidden: data.hidden || false,
+	};
 }
