bf54811e04023adb2ff2f0c147674cd6ea54a26c
J4F3ET <jslopezz@udistrital.edu.co>
2024-12-23 08:52:04 -0500

    style: :lipstick: Refactor script and link loading in home screen for improved performance and organization

--- src/view/dist/js/home-script.js
+++ src/view/dist/js/home-script.js
@@ -30,23 +30,6 @@ window.onscroll = () => {
 	menuIcon.classList.remove("bx-x");
 	navbar.classList.remove("active-link");
 };
-
-/*= SCROLL REVEAL =*/
-// ScrollReveal({
-// 	//reset: true,
-// 	distance: "80px",
-// 	duration: 2000,
-// 	delay: 200,
-// });
-
-// ScrollReveal().reveal(".home-content, .heading", {origin: "top"});
-// ScrollReveal().reveal(
-// 	".home-img, .levels-container, .overview-content, .about-container",
-// 	{origin: "bottom"}
-// );
-// ScrollReveal().reveal(".home-content h1, .overview-img", {origin: "left"});
-// ScrollReveal().reveal(".home-content p, .overview-content", {origin: "right"});
-
 /*= TYPED JS =*/
 const typed = new Typed(".multiple-text", {
 	strings: ["have fun!", "learn!", "work in teams!", "interact!", "practice!"],
