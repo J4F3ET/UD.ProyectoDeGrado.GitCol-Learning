346882c2dba922dbbe0cdbe41a3ace6c59987bdb
J4F3ET <jslopezz@udistrital.edu.co>
2025-02-13 17:36:55 -0500

    feat: :sparkles: Add login dialog and functionality with Firebase authentication for improved user experience

--- src/view/dist/js/alone-mode-script.js
+++ src/view/dist/js/alone-mode-script.js
@@ -1,5 +1,6 @@
-import {driveUsageMode} from "./drivejs-mode-script.js";
-
+import { driveUsageMode } from "./drivejs-mode-script.js";
+import { auth } from "./firebase-config.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 document.getElementById("btnExit").addEventListener("click", () => {
 	window.location.href = "/";
 });
@@ -15,3 +16,25 @@ document.getElementById("btnTutorial").addEventListener("click", () => {
 		},
 	}).drive();
 });
+
+
+// Handle login and logout buttons
+const btnDisable = async (btn) => {
+	btn.disabled = true;
+	btn.style.display = "none";
+};
+const btnEnable = async (btn) => {
+	btn.disabled = false;
+	btn.style.display = "block";
+};
+const resolveBtnLogin = async (user) => {
+	if (user) {
+		btnDisable(document.getElementById("btn_login"));
+		btnEnable(document.getElementById("btnLogout"));
+	} else {
+		btnEnable(document.getElementById("btn_login"));
+		btnDisable(document.getElementById("btnLogout"));
+	}
+};
+
+onAuthStateChanged(auth, async (user) => resolveBtnLogin(user));
