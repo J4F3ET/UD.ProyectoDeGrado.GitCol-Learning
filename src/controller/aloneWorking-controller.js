4866758bd06733427f674060608dccd51ed87772
J4F3ET <jslopezz@udistrital.edu.co>
2025-03-10 20:30:31 -0500

    feat: :sparkles: Add concept retrieval and render in alone mode view

--- src/controller/aloneWorking-controller.js
+++ src/controller/aloneWorking-controller.js
@@ -1,8 +1,9 @@
-import {Router} from "express";
+import { Router } from "express";
+import { getConcept } from "../model/concept-service.js";
 const router = Router();
 /**
  * @openapi
- * /aloneMode:
+ * /aloneMode/{name}:
  *   get:
  *     summary: Endpoint render view alone-mode
  *     description: Render view alone-mode
@@ -14,8 +15,11 @@ const router = Router();
  *             example: alone-mode-screen.ejs
  */
 router.get("/aloneMode/:name", async (req, res) => {
-    const conceptName = req.params.name;
-    console.log(conceptName);
-    res.render("alone-mode-screen")
+	const conceptName = req.params.name;
+    if (!conceptName) return res.sendStatus(404);
+	const concept = await getConcept(conceptName);
+	if (!concept) return res.sendStatus(404);
+
+	res.render("alone-mode-screen", { concept });
 });
 export default router;
--- src/model/concept-service.js
+++ src/model/concept-service.js
@@ -1,6 +1,6 @@
 import { database } from "./firebase-service";
 
-const getConcept = async (key) => {
+export const getConcept = async (key) => {
 	try {
 		const snapshot = await database.ref("concepts/" + key).once("value");
 		return snapshot.val();
