0bf57d07fe1b89956b7fb591d316bb0008c7333e
J4F3ET <jslopezz@udistrital.edu.co>
2025-03-31 21:38:43 -0500

    feat: :sparkles: Add concept selection dropdown and dialog for question display in alone mode

---
+++ src/view/dist/js/dialogs/dialog-question-concept-script.js
@@ -0,0 +1,14 @@
+const dialog_question = document.querySelector("#dialog_question");
+document.getElementById("btnTest").addEventListener("click", async () => {
+	openDialogQuestion();
+});
+document
+	.getElementById("btn_close_question")
+	.addEventListener("click", async () => await closeDialogQuestion());
+const openDialogQuestion = async () => {
+	console.log(dialog_question);
+	dialog_question.showModal();
+};
+const closeDialogQuestion = async () => {
+	dialog_question.close();
+};
--- src/view/dist/js/utils/concept-config.js
+++ src/view/dist/js/utils/concept-config.js
@@ -1,43 +1,35 @@
-import { driverQuestionsChallenger } from "../drivejs-mode-script.js";
 import { logConceptChallenge } from "../mode-script.js";
-document.getElementById("btnTest").addEventListener("click", async () => {
-    driverQuestionsChallenger(
-        ["A", "B", "C", "D", "E"],
-        "What is the capital of Spain?",
-        "Question"
-    ).drive();
-});
 export const changeConcept = async (concept) => {
-    if (!concept) return;
-    const statusChangeConcept = sessionStorage.getItem("statusChangeConcept");
-    if (statusChangeConcept) return;
+	if (!concept) return;
+	const statusChangeConcept = sessionStorage.getItem("statusChangeConcept");
+	if (statusChangeConcept) return;
 	changeChallengeConcept(concept.challenger);
 	changeQuestionConcept(concept.question);
-    sessionStorage.setItem("statusChangeConcept", 1);
+	sessionStorage.setItem("statusChangeConcept", 1);
 };
 const changeChallengeConcept = async (challenger) => {
-    const { explanation, steps, log } = challenger;
-    changeAllCommandsConcept(steps);
-    changeAllLogsConcept([{ message: explanation,tag: "explanation" }, ...log]);
+	const { explanation, steps, log } = challenger;
+	changeAllCommandsConcept(steps);
+	changeAllLogsConcept([{ message: explanation, tag: "explanation" }, ...log]);
 };
 const changeAllLogsConcept = async (logs) => {
-    if (!logs) return;
-    logs.forEach((log) => changeLogConcept(log));
+	if (!logs) return;
+	logs.forEach((log) => changeLogConcept(log));
 };
 const changeLogConcept = async ({ tag, message }) => {
-    if (!message) return;
-    logConceptChallenge(tag, message);
+	if (!message) return;
+	logConceptChallenge(tag, message);
 };
 const changeAllCommandsConcept = async (steps) => {
-    if (!steps) return;
-    steps.forEach((step) => changeCommandConcept(step));
+	if (!steps) return;
+	steps.forEach((step) => changeCommandConcept(step));
 };
 const changeCommandConcept = async (command) => {
-    const input = document.getElementById("comandInput");
-    input.value = command;
-    input.dispatchEvent(
-        new KeyboardEvent("keyup", { key: "Enter", keyCode: 13 })
-    );
+	const input = document.getElementById("comandInput");
+	input.value = command;
+	input.dispatchEvent(
+		new KeyboardEvent("keyup", { key: "Enter", keyCode: 13 })
+	);
 };
 //Esto se debe activar al momento de querer salir o de cambiar de reto
 const changeQuestionConcept = async (question) => {};
