09e93992487952723b77e7317aeb4c85844eee42
J4F3ET <jslopezz@udistrital.edu.co>
2025-05-21 20:15:30 -0500

    feat: :sparkles: Mejorar formato y consistencia en la función de inicio de sesión y agregar manejo de errores en el endpoint de cookie

--- src/controller/login-controller.js
+++ src/controller/login-controller.js
@@ -36,21 +36,21 @@ const router = Router();
  *       - cookieAuth: []
  */
 router.post("/login", releaseVerificationMiddleware, async (req, res) => {
-	const { uid,email } = req.body;
+	const { uid, email } = req.body;
 	if (!uid || uid == "") {
 		res
 			.clearCookie("access_token")
 			.status(HttpStatus.NOT_FOUND)
-			.json({ url: "/error" ,status: "error" })
+			.json({ url: "/error", status: "error" })
 			.end();
 		return;
 	}
-	const { err,data } = await userLogin(uid,email);
+	const { err, data } = await userLogin(uid, email);
 	if (err && data.message === "Email not found") {
 		res
 			.clearCookie("access_token")
 			.status(HttpStatus.UNAUTHORIZED)
-			.json({status: "pending"})
+			.json({ status: "pending" })
 			.end();
 		return;
 	}
@@ -58,11 +58,11 @@ router.post("/login", releaseVerificationMiddleware, async (req, res) => {
 		res
 			.clearCookie("access_token")
 			.status(HttpStatus.UNAUTHORIZED)
-			.json({ url: "/error",status: "error" })
+			.json({ url: "/error", status: "error" })
 			.end();
 		return;
 	}
-	res.status(HttpStatus.OK).json({ url: "/rooms",status: "success" });
+	res.status(HttpStatus.OK).json({ url: "/rooms", status: "success" });
 	res.end();
 });
 /**
@@ -88,8 +88,21 @@ router.post("/login", releaseVerificationMiddleware, async (req, res) => {
  *           text/html:
  *             example: error-screen.ejs
  */
-router.get("/logout", releaseVerificationMiddleware, (req, res) => {
+router.get("/logout", releaseVerificationMiddleware,  (req, res) => {
 	res.clearCookie("access_token");
 	res.end();
+});
+
+router.get("/login/cookie", async (req, res) => {
+	if (req.headers.cookie == undefined) {
+		res.status(HttpStatus.UNAUTHORIZED).json({ status: "error" });
+		return;
+	}
+	const token = req.headers.cookie.split("=")[1];
+	if (token == undefined) {
+		res.status(HttpStatus.UNAUTHORIZED).json({ status: "error" });
+		return;
+	}
+	res.status(HttpStatus.OK).json({ status: "success" }).end();
 });
 export default router;
